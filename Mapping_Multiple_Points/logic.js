49870ca452529e124a10e7eb2348242b62950966
atlantys <79942792+rryl@users.noreply.github.com>
2022-08-19 11:13:18 -0500

    Add files via upload

---
+++ Mapping_Multiple_Points/logic.js
@@ -0,0 +1,23 @@
+console.log("working");
+
+let map = L.map('mapid').setView([40.7, -94.5], 4);
+
+let cityData = cities;
+
+cityData.forEach(function(city) {
+    console.log(city)
+    L.circleMarker(city.location)
+    .bindPopup("<h2>" + city.city + ", " + city.state + "</h2> <hr> <h3>Population " + city.population + "</h3>")
+  .addTo(map);
+});
+
+let streets = L.tileLayer('https://api.mapbox.com/styles/v1/mapbox/dark-v10/tiles/{z}/{x}/{y}?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, <a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: API_KEY
+});
+
+streets.addTo(map);
